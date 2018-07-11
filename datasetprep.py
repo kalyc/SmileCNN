205b0cb52787051e7556a0602085af2b8622c881
kalyc <ubuntu@ip-172-31-91-19.ec2.internal>
2018-07-11 23:39:53 +0000

    Use mxnet util to download dataset

--- datasetprep.py
+++ datasetprep.py
@@ -1,9 +1,20 @@
 import mxnet as mx
 import numpy as np
+import zipfile
+from utils import list_all_files
 
+
+# Retrieve dataset files
+url = 'https://github.com/hromi/SMILEsmileD/archive/master.zip'
+
+print("Downloading dataset")
+mx.test_utils.download(url)
+
+zip_ref = zipfile.ZipFile('master.zip', 'r')
+zip_ref.extractall()
+zip_ref.close()
 
 # Load training images
-from utils import list_all_files
 negative_paths = list(list_all_files('SMILEsmileD-master/SMILEs/negatives/negatives7/', ['.jpg']))
 print('loaded', len(negative_paths), 'negative examples')
 positive_paths = list(list_all_files('SMILEsmileD-master/SMILEs/positives/positives7/', ['.jpg']))
